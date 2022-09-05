585be30e4837fcc8d261313343ff9fa0a9e230a9
Matheus Willian <matheuswilliandf@gmail.com>
2022-09-05 12:03:59 -0300

    Ajustes na questão 10 de decisão

--- Exercicios_Python/02_EstruturaDecisao/q10.py
+++ Exercicios_Python/02_EstruturaDecisao/q10.py
@@ -4,12 +4,15 @@ Peça para digitar M-matutino ou V-Vespertino ou N- Noturno.
 Imprima a mensagem "Bom Dia!", "Boa Tarde!" ou "Boa Noite!" ou "Valor Inválido!", conforme o caso.
 '''
 
-turno = input('Digite o turno: ')
-if turno.upper() == 'M':
+turno = input('Digite o turno: ').upper()
+#turno = turno.upper()
+
+if turno == 'M':
     print('Bom Dia!')
-elif turno.upper() == 'V':
+elif turno == 'V':
+#elif turno.lower() == 'v':
     print('Bom Tarde!')
-elif turno.upper() == 'N':
+elif turno == 'N':
     print('Bom Noite!')
 else:
     print('Valor inválido!')
