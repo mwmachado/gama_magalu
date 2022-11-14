acd7cac4777b18b523728c8e44c144f9cd6d82a9
Matheus Willian <matheuswilliandf@gmail.com>
2022-11-14 18:02:05 -0300

    Adicionado API de Python com SQLite

---
+++ Aulas/API/SQLite/api.py
@@ -0,0 +1,108 @@
+from flask import Flask, redirect, url_for, request, render_template
+import sqlite3 as sql
+
+app = Flask(__name__)
+banco = 'gama.bd'
+
+# Função
+def abrir_conexao(banco):
+    conexao = sql.connect(banco)
+    cursor = conexao.cursor()
+    return conexao, cursor
+
+def fechar_conexao(conexao):
+    conexao.commit()
+    conexao.close()
+
+# Comandos SQL
+contagem = "SELECT COUNT(*) FROM alunos;"
+select_todos = "SELECT * FROM alunos;"
+select_nome = "SELECT * FROM alunos WHERE nome like ?"
+insert = "INSERT INTO alunos VALUES (:nome,:idade,:filhos,:estado,:altura,:formacao)"
+truncate = "DELETE FROM alunos;" #"TRUNCATE TABLE alunos;"
+delete_nome = "DELETE FROM alunos WHERE nome like ?"
+update = '''
+UPDATE alunos SET
+    nome = :nome,
+    idade = :idade,
+    filhos = :filhos,
+    estado = :estado,
+    altura = :altura,
+    formacao = :formacao
+WHERE nome like :nome  
+'''
+
+
+# index
+@app.route('/')
+def index():
+    conexao, cursor = abrir_conexao(banco)
+    resultado = cursor.execute(contagem).fetchone()
+    fechar_conexao(conexao)
+    return {'registros': f'{resultado[0]} alunos'}
+
+# read all
+@app.route('/read')
+def read():
+    conexao, cursor = abrir_conexao(banco)
+    resultado = cursor.execute(select_todos).fetchall() #[(aluno1, ...), (aluno2, ...)]
+    fechar_conexao(conexao)
+    return resultado
+
+# read por nome
+@app.route('/read/<nome>')
+def read_name(nome):
+    conexao, cursor = abrir_conexao(banco)
+    resultado = cursor.execute(select_nome, [nome]).fetchall()
+    fechar_conexao(conexao)
+    return resultado
+
+# create
+@app.route('/create')
+#?nome=teste&idade=0&filhos=1&estado=AC&altura=0.07&formacao=Ensino+Superior
+def create():
+    aluno=request.args.to_dict() #{chave: valor, chave:valor,...}
+    if aluno: # se tem argumento
+        conexao, cursor = abrir_conexao(banco)
+        cursor.execute(insert, aluno)
+        fechar_conexao(conexao)
+        return aluno
+    else: # se não tem argumento
+        return redirect(url_for('static', filename='cadastro.html'))
+
+# delete nome
+@app.route('/delete/<nome>')
+def delete_name(nome):
+    conexao, cursor = abrir_conexao(banco)
+    resultado = cursor.execute(delete_nome, [nome]).rowcount
+    fechar_conexao(conexao)
+    return {'message': f'{resultado} aluno(s) foram removido(s)!'}
+
+# delete all
+@app.route('/delete')
+def delete_all():
+    conexao, cursor = abrir_conexao(banco)
+    cursor.execute(truncate)
+    fechar_conexao(conexao)
+    return {'message': 'Todos os alunos foram apagados!'}
+
+# update
+@app.route('/update/<nome>')
+#?nome=teste&idade=0&filhos=1&estado=AC&altura=0.07&formacao=Ensino+Superior
+def update_name(nome):
+    consulta = read_name(nome)
+    if consulta:
+        aluno=request.args.to_dict() #{chave: valor, chave:valor,...}
+        if aluno: # se tem argumento
+            conexao, cursor = abrir_conexao(banco)
+            cursor.execute(update, aluno)
+            fechar_conexao(conexao)
+            return aluno
+        else: # se não tem argumento
+            return render_template('atualizacao.html', aluno=consulta[0])
+    else:
+        return {'error': 'Aluno não encontrado!'}
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- Aulas/API/SQLite/main.py
+++ Aulas/API/SQLite/main.py
@@ -5,16 +5,17 @@ conexao = sql.connect('projeto.db')
 # Cursor e Executando Comandos
 cursor = conexao.cursor()
 # ## Criar tabela
-# cursor.execute('''
-# CREATE TABLE alunos(
-#     nome,
-#     idade,
-#     filhos,
-#     estado,
-#     altura,
-#     formacao
-# );
-# ''')
+cursor.execute('''DROP TABLE alunos;''')
+cursor.execute('''
+CREATE TABLE alunos(
+    nome text,
+    idade int,
+    filhos int,
+    estado text,
+    altura real,
+    formacao text
+);
+''')
 
 # Lista todas as tabelas do banco de dados
 cursor.execute('''
@@ -24,110 +25,110 @@ cursor.execute('''
 ''')
 print(cursor.fetchall())
 
-# ## Inserir dados
-# cursor.execute('''
-# INSERT INTO alunos VALUES
-#     ('Anderson',40,0,'SP',1.90,'Superior Incompleto (ADS)'),
-#     ('Ettore Mitsueda',29,2,'SP',1.72,'Ensino Médio Completo'),
-#     ('Bruna',28,0,'MG',1.62,'Superior completo'),
-#     ('clara',19,0,'RJ',1.63,'superior incompleto (ADS)'),
-#     ('João Paulo',27,0,'DF',1.50,'Superior completo'),
-#     ('Fernando A',32,1,'SP',1.74,'Mestrado completo/Doutorado incompleto'),
-#     ('José Vitor',24,0,'PB',1.78,'superior incompleto'),
-#     ('Raissa',32,2,'GO',1.68,'Superior'),
-#     ('George',38,0,'CE',1.80,'Superior'),
-#     ('Bruno Coelho',30,0,'SP',1.70,'Superior '),
-#     ('Fernando Oliveira',41,0,'SP',1.79,'Superior'),
-#     ('Edson',26,0,'PR',1.80,'Superior Incompleto'),
-#     ('Eron',32,1,'SP',1.76,'Ensino Médio Completo'),
-#     ('Fernando',23,0,'RN',1.70,'Ensino Médio'),
-#     ('Wagner',30,0,'CE',1.73,'Superior Completo'),
-#     ('Gabrielle',25,0,'RJ',1.56,'Superior Completo'),
-#     ('Larissa',28,0,'RJ',1.74,'Ensino Superior Completo'),
-#     ('Câmara',58,1,'RJ',1.80,'Superior Completo'),
-#     ('William Ferrari',40,3,'SP',1.73,'Superior incompleto'),
-#     ('Erick',26,0,'SP',1.70,'Ensino Superior Completo'),
-#     ('lucas',26,0,'SP',1.70,'superior'),
-#     ('Willian Caetano',37,1,'pa',1.75,'superior'),
-#     ('Divino',24,0,'SP',1.69,'Superior'),
-#     ('Natalia',25,0,'SS',1.59,'Ensino Médio Completo'),
-#     ('julia',30,0,'pa',1.59,'superior completo'),
-#     (' Gabriel Silva',27,0,'SP',1.80,'superior incompleto'),
-#     ('Bruno Fernandes',25,0,'SC',1.90,'Superior incompleto'),
-#     ('Josivaldo',35,8,'SP',1.80,'Ensino medio Completo'),
-#     ('BRUNO LIMA',32,0,'pr',1.80,'Pós graduado'),
-#     ('Sandro Santos Marra',38,0,'DF',1.83,'Ensino Superior Inc'),
-#     ('Gabriel Queiroz',23,0,'MG',1.78,'Superior Completo')
-# ;''')
-# conexao.commit() # Salva as alterações no banco de dados
+## Inserir dados
+cursor.execute('''
+INSERT INTO alunos VALUES
+    ('Anderson',40,0,'SP',1.90,'Superior Incompleto (ADS)'),
+    ('Ettore Mitsueda',29,2,'SP',1.72,'Ensino Médio Completo'),
+    ('Bruna',28,0,'MG',1.62,'Superior completo'),
+    ('clara',19,0,'RJ',1.63,'superior incompleto (ADS)'),
+    ('João Paulo',27,0,'DF',1.50,'Superior completo'),
+    ('Fernando A',32,1,'SP',1.74,'Mestrado completo/Doutorado incompleto'),
+    ('José Vitor',24,0,'PB',1.78,'superior incompleto'),
+    ('Raissa',32,2,'GO',1.68,'Superior'),
+    ('George',38,0,'CE',1.80,'Superior'),
+    ('Bruno Coelho',30,0,'SP',1.70,'Superior '),
+    ('Fernando Oliveira',41,0,'SP',1.79,'Superior'),
+    ('Edson',26,0,'PR',1.80,'Superior Incompleto'),
+    ('Eron',32,1,'SP',1.76,'Ensino Médio Completo'),
+    ('Fernando',23,0,'RN',1.70,'Ensino Médio'),
+    ('Wagner',30,0,'CE',1.73,'Superior Completo'),
+    ('Gabrielle',25,0,'RJ',1.56,'Superior Completo'),
+    ('Larissa',28,0,'RJ',1.74,'Ensino Superior Completo'),
+    ('Câmara',58,1,'RJ',1.80,'Superior Completo'),
+    ('William Ferrari',40,3,'SP',1.73,'Superior incompleto'),
+    ('Erick',26,0,'SP',1.70,'Ensino Superior Completo'),
+    ('lucas',26,0,'SP',1.70,'superior'),
+    ('Willian Caetano',37,1,'pa',1.75,'superior'),
+    ('Divino',24,0,'SP',1.69,'Superior'),
+    ('Natalia',25,0,'SS',1.59,'Ensino Médio Completo'),
+    ('julia',30,0,'pa',1.59,'superior completo'),
+    (' Gabriel Silva',27,0,'SP',1.80,'superior incompleto'),
+    ('Bruno Fernandes',25,0,'SC',1.90,'Superior incompleto'),
+    ('Josivaldo',35,8,'SP',1.80,'Ensino medio Completo'),
+    ('BRUNO LIMA',32,0,'pr',1.80,'Pós graduado'),
+    ('Sandro Santos Marra',38,0,'DF',1.83,'Ensino Superior Inc'),
+    ('Gabriel Queiroz',23,0,'MG',1.78,'Superior Completo')
+;''')
+conexao.commit() # Salva as alterações no banco de dados
 cursor.execute("select count(*) from alunos;")
 print(cursor.fetchall())
 # conexao.rollback() # Descarta as alterações
 # cursor.execute("select count(*) from alunos;")
 # print(cursor.fetchall())
 
-## Consultar dados
-cursor.execute('''
-SELECT *
-FROM alunos
-WHERE idade > 40
-;''')
+# ## Consultar dados
+# cursor.execute('''
+# SELECT *
+# FROM alunos
+# WHERE idade > 40
+# ;''')
 
-print(cursor.fetchone())
+# print(cursor.fetchone())
 
-## Inserindo múltiplas linhas
-data = [
-    ('Matheus',23,0,'MG',1.78,'Superior Completo'),
-    ('Ana',23,0,'MG',1.78,'Superior Completo')
-]
+# ## Inserindo múltiplas linhas
+# data = [
+#     ('Matheus',23,0,'MG',1.78,'Superior Completo'),
+#     ('Ana',23,0,'MG',1.78,'Superior Completo')
+# ]
 
-'''
-INSERT INTO alunos VALUES
-('Matheus',23,0,'MG',1.78,'Superior Completo')
-'''
-'''
-INSERT INTO alunos VALUES
-('Ana',23,0,'MG',1.78,'Superior Completo')
-'''
+# '''
+# INSERT INTO alunos VALUES
+# ('Matheus',23,0,'MG',1.78,'Superior Completo')
+# '''
+# '''
+# INSERT INTO alunos VALUES
+# ('Ana',23,0,'MG',1.78,'Superior Completo')
+# '''
 
-# Evita o SQLInjection
-cursor.executemany(f'''
-INSERT INTO alunos VALUES
-(?,?,?,?,?,?)
-''', data)
+# # Evita o SQLInjection
+# cursor.executemany(f'''
+# INSERT INTO alunos VALUES
+# (?,?,?,?,?,?)
+# ''', data)
 
-data = [
-    {
-        'nome':'Matheus',
-        'idade':23,
-        'filhos':0,
-        'estado':'MG',
-        'altura':1.78,
-        'formacao':'Superior Completo'
-    },
-    {
-        'nome':'Ana',
-        'idade':23,
-        'filhos':0,
-        'estado':'MG',
-        'altura':1.78,
-        'formacao':'Superior Completo'
-    }
-]
-cursor.executemany(f'''
-INSERT INTO alunos VALUES
-(:nome,:idade,:filhos,:estado,:altura,:formacao)
-''', data)
-'''
-INSERT INTO alunos VALUES
-('Matheus',23,0,'MG',1.78,'Superior Completo')
-'''
-'''
-INSERT INTO alunos VALUES
-('Ana',23,0,'MG',1.78,'Superior Completo')
-'''
+# data = [
+#     {
+#         'nome':'Matheus',
+#         'idade':23,
+#         'filhos':0,
+#         'estado':'MG',
+#         'altura':1.78,
+#         'formacao':'Superior Completo'
+#     },
+#     {
+#         'nome':'Ana',
+#         'idade':23,
+#         'filhos':0,
+#         'estado':'MG',
+#         'altura':1.78,
+#         'formacao':'Superior Completo'
+#     }
+# ]
+# cursor.executemany(f'''
+# INSERT INTO alunos VALUES
+# (:nome,:idade,:filhos,:estado,:altura,:formacao)
+# ''', data)
+# '''
+# INSERT INTO alunos VALUES
+# ('Matheus',23,0,'MG',1.78,'Superior Completo')
+# '''
+# '''
+# INSERT INTO alunos VALUES
+# ('Ana',23,0,'MG',1.78,'Superior Completo')
+# '''
 
-cursor.commit()
+# cursor.commit()
 
 # Fechar Conexão
 conexao.close()
