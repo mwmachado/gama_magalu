1df7ff5c14a657d86c9ca01a43c76f93a093eabf
Matheus Willian <matheuswilliandf@gmail.com>
2022-11-16 15:23:28 -0300

    Adicionado básico de mysql com python

---
+++ Aulas/API/MySQL/main.py
@@ -0,0 +1,16 @@
+#pip install mysql-connector-python
+import mysql.connector as sql
+
+conexao = sql.connect(
+    host="127.0.0.1",
+    user="root",
+    password="gama1234",
+    database="filmes"
+)
+cursor = conexao.cursor()
+
+cursor.execute("SELECT * FROM usuarios;")
+resultado = cursor.fetchall()
+
+print(resultado)
+conexao.close()
